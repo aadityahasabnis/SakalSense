8509050665ff7133aa468781afcb74dd21ec7dc7
aadityahasabnis <aaditya.hasabnis@gmail.com>
2026-01-13 21:13:43 +0530

    fix: update Prisma type import for v7 compatibility

--- apps/frontend/src/server/actions/content/courseActions.ts
+++ apps/frontend/src/server/actions/content/courseActions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { type Prisma } from '@prisma/client';
+import { type Prisma } from '.prisma/client';
 
 import { STAKEHOLDER } from '@/constants/auth.constants';
 import { type ContentStatusType, type DifficultyType } from '@/constants/content.constants';
